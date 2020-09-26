1dbe4e93bb68f4a1eabb2f26c8dc8d0b74efc384
Taylor Zanke <taylor.zanke@gmail.com>
2020-09-26 10:54:25 -0700

    pause toggler

--- src/components/PageContainer/PageContainer.js
+++ src/components/PageContainer/PageContainer.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './PageContainer.scss';
 import Grid from '@material-ui/core/Grid'
 
-const PageContainer = ({ children, className }) => (
-    <Grid container className={`${className && className} page-container`} justify="center">
-        {children}
-    </Grid>
-)
+const PageContainer = ({ children, className }) => {
+    const [isPaused, setIsPaused] = useState(false)
+    return (
+        <>
+            <Grid container className={`${className && className} ${isPaused && "is-paused"} page-container`} justify="center">
+                {children}
+            </Grid>
+            <div className="pause-toggler" onClick={() => setIsPaused(!isPaused)}>
+                {isPaused ? "START" : "PAUSE"}
+            </div>
+        </>
+    )
+}
 
 export default PageContainer
